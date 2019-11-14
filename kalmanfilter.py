68eae71c37ce9f9d3e0d841d42880d3d5d483cbe
1kek0u <1kek0u.1kd@gmail.com>
2019-11-14 23:27:36 +0900

    kalmanfilter

--- kalmanfilter.py
+++ kalmanfilter.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def kalmanfilter_2d(d:pd.core.frame.DataFrame,*,x:str='x',y:str='y', return_MSE:bool = False,
+def kalmanfilter_2d(x,y,t,*,return_MSE:bool = False,
                     R = np.matrix([[0.420905,0,0,0],[0,1.24298,0,0],[0,0,0.420905,0],[0,0,0,1.24298]]),
                     Q = np.matrix([[0.21,0,0,0],[0,0.31,0,0],[0,0,0.21,0],[0,0,0,0.31]]),
                     P0 = np.matrix([[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]])):
@@ -10,41 +10,39 @@ def kalmanfilter_2d(d:pd.core.frame.DataFrame,*,x:str='x',y:str='y', return_MSE:
     H = np.matrix([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]])
     xyMSE = 0
     P = P0
-    f_d = pd.DataFrame(index = d.index , columns = d.columns)
-    for j in range(len(d.columns)):
-        if d.columns[j] != x and d.columns[j] != y:
-            f_d.iloc[:,j] = d.iloc[:,j]
-    f_d.at[0,x] = d.at[0,x]
-    f_d.at[0,y] = d.at[0,y]
-    Xhat = np.matrix([[f_d.at[0,x]],[f_d.at[0,y]],[0],[0]])
-    for i in range(1,len(d)):
-        dt =(d.at[i,'t'] - d.at[i-1,'t'])/1000
+    f_x = np.zeros(len(x))
+    f_y = np.zeros(len(y))
+    f_x[0] = x[0]
+    f_y[0] = y[0]
+    Xhat = np.matrix([[f_x[0]],[f_y[0]],[0],[0]])
+    for i in range(1,len(t)):
+        dt =(t[i] - t[i-1])/1000
         if dt < 15 :
             F = np.matrix([[1,0,dt,0],[0,1,0,dt],[0,0,1,0],[0,0,0,1]])
             Xhat_k1 = np.dot(F,Xhat)
             P = np.dot(np.dot(F,P),F.T) + np.dot(np.dot(G,Q),G.T)
-            Zvx = (d.at[i,x]-d.at[i-1,x])/dt
-            Zvy = (d.at[i,y]-d.at[i-1,y])/dt
-            Z = np.matrix([[d.at[i,x]],[d.at[i,y]],[Zvx],[Zvy]])
+            Zvx = (x[i]-x[i-1])/dt
+            Zvy = (y[i]-y[i-1])/dt
+            Z = np.matrix([[x[i]],[y[i]],[Zvx],[Zvy]])
             e = Z - np.dot(H,Xhat_k1)
             S = R + np.dot(np.dot(H,P),H.T)
             K = np.dot(np.dot(P,H),np.linalg.inv(S))
             Xhat = Xhat_k1 + np.dot(K,e)
             P = np.dot((I-np.dot(K,H)),P)
-            f_d.at[i,x] = Xhat[0,0]
-            f_d.at[i,y] = Xhat[1,0]
+            f_x[i] = Xhat[0,0]
+            f_y[i] = Xhat[1,0]
             if return_MSE:
                 xyMSE = xyMSE+((Xhat[0,0]-Xhat_k1[0,0])**2)+((Xhat[1,0]-Xhat_k1[1,0])**2)+((Xhat[0,0]-Z[0,0])**2)+((Xhat[1,0]-Z[1,0])**2)
         else:
             P = P0
-            f_d.at[i,x] = d.at[i,x]
-            f_d.at[i,y] = d.at[i,y]
-            Xhat = np.matrix([[f_d.at[i,x]],[f_d.at[i,y]],[0],[0]])
+            f_x[i] = x[i]
+            f_y[i] = y[i]
+            Xhat = np.matrix([[f_x[i]],[f_y[i]],[0],[0]])
     if return_MSE:
-        xyMSE = xyMSE / len(d)
-        return f_d,xyMSE
+        xyMSE = xyMSE / len(t)
+        return f_x,f_y,xyMSE
     else:
-        return f_d
+        return f_x,f_y
 
 
 def kalmanfilter_3d(d:pd.core.frame.DataFrame,*,x:str='x',y:str='y',z:str='z', return_MSE:bool = False,
